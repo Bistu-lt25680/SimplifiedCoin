9d7249f680a3db005d0c4824f7da3e5cbc0f807e
Liu Tian <144797653+Bistu-lt25680@users.noreply.github.com>
2024-10-09 15:58:57 +0800

    Delete test/1.ts

--- test/1.ts
+++
@@ -1,8 +0,0 @@
-import {Chain, Block} from ".";
-
-const chain = new Chain();
-const block1 = new Block("转账10元", "123");
-
-chain.addBlockToChain(block1);
-console.log("test1");
-console.log(chain);
